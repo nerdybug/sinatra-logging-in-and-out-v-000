a94815cbbac2e908c0ab3d8b94f9c508223b7f80
Meredith Reece <mardebugger@gmail.com>
2018-02-19 21:42:27 +0000

    automatically backed up by learn

--- db/migrate/20180219213951_create_users.rb
+++ db/migrate/20180219213951_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.integer :integer
+      t.integer :balance
     end
   end
 end
